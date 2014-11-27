8df53590fd5ce7a239dd9eec58dc864373441ab0
Michele Carignani <michele.carignani@gmail.com>
2014-11-27 12:50:11 +0100

    migliore etichetta per "tutte le regioni"

--- dreamers-sfide/regioni_e_zone_sample.php
+++ dreamers-sfide/regioni_e_zone_sample.php
@@ -2,7 +2,7 @@
 
 // CREG,NOME,NOME_SHORT,AREA
 $regioni = array(
-	array("A","TUTTE LE REGIONI","CM_NAZ",0),
+	array("A","-- TUTTE LE REGIONI --","CM_NAZ",0),
 	array("B","ABRUZZO","ABR",3),
 	array("C","BASILICATA","BAS",4),
 	array("D","CALABRIA","CAL",4),
@@ -11,7 +11,7 @@ $regioni = array(
 
 $zone = array(
 	//CREG,CZONA,NOME
-	array("A",1,"TUTTE LE ZONE"),
+	array("A",1,"-- TUTTE LE ZONE --"),
 	array("B",1,"C.Z. CHIETI"),
 	array("B",3,"C.Z. PESCARA"),
 	array("C",1,"C.Z. MATERA POTENZA"),
