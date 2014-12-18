3142441b537ba97ec9ad8dc4c2c562da405cdedc
Stefano Tamagnini <yoghi@sigmalab.net>
2014-12-19 00:19:33 +0100

    rimozione Dashboard -> I miei siti

--- dreamers-game/dreamers-game.php
+++ dreamers-game/dreamers-game.php
@@ -71,6 +71,9 @@ function remove_wp_dashboard_widgets() {
     wp_unregister_sidebar_widget( 'dashboard_activity' ); 
     remove_meta_box( 'dashboard_activity', 'dashboard', 'normal');//since 3.8
 
+    //MIEI SITI
+    remove_submenu_page('index.php', 'my-sites');
+
   }
 
 }
