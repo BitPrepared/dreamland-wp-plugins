5f55ea4454f8d3b3863aab063d718ee20deffab9
Michele Carignani <michele.carignani@gmail.com>
2015-02-18 01:38:32 +0100

    fix

--- dreamers-sfide/dreamers-sfide-utils.php
+++ dreamers-sfide/dreamers-sfide-utils.php
@@ -206,7 +206,7 @@ function rtd_completa_sfida($sfida, $user_id = NULL, $is_sfida, $tiposfida, $sup
     // Salva connessione con la sfida
     add_post_meta($new_post_id, 'sfida', $sfida->ID, True);
 
-    $cats = wp_get_post_terms($sfida->ID, 'tipologiesifde', array("fields" => "ids"));
+    $cats = wp_get_object_terms($sfida->ID, 'tipologiesfide', array("fields" => "ids"));
     wp_set_object_terms($new_post_id, $cats, 'tipologiesfide', true);
 
     // Tieni traccia delle missioni
